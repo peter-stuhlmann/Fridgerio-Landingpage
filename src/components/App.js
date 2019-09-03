8b41a21bf7b03c15aeba1ffb847a0c0bd8ea07e5
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-09-03 23:29:22 +0200

    Translate route paths into german

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import LegalNotice from './LegalNotice';
+import PrivacyPolicy from './PrivacyPolicy';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -7,8 +11,8 @@ function App() {
       <main>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/legalnotice" component={LegalNotice} />
-          <Route path="/privacypolicy" component={PrivacyPolicy} />
+          <Route path="/impressum" component={LegalNotice} />
+          <Route path="/datenschutzerklaerung" component={PrivacyPolicy} />
           <Route component={NotFound} />
         </Switch>
       </main>
