f19c7e67ac895ae4b92f19416750b05d1d9d1336
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-09-03 23:15:31 +0200

    Add router
    Add routes for Homepage, legal notice, privacy policy and the Error 404 page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <Fragment>Fridgerio App</Fragment>;
+  return (
+    <BrowserRouter>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/legalnotice" component={LegalNotice} />
+          <Route path="/privacypolicy" component={PrivacyPolicy} />
+          <Route component={NotFound} />
+        </Switch>
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
