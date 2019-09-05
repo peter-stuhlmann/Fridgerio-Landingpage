79c4569e12694188c5405875d12e94f81a84d4d0
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-09-05 22:13:02 +0200

    Add wrapper and main to global styles

--- src/components/styled-components/GlobalStyles.js
+++ src/components/styled-components/GlobalStyles.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
-import { FontSize } from './Variables';
+import { FontSize, Colors, ScreenSize } from './Variables';
 
 const StyledGlobalStyles = createGlobalStyle`
   body {
     margin: 0;
     font-size: ${props => props.size || FontSize.normal};
+    background-color: ${props => props.background || Colors.LightColor};
+
+    main {
+      width: 100%;
+      max-width: ${ScreenSize.Desktop};
+      margin: 30px auto;
+    }
+
+    .wrapper {
+      display: flex;
+      justify-content: space-between;
+      flex-flow: wrap row;
+      width: 100%;
+      max-width: 1170px;
+      margin: 0 auto;
+      box-sizing: border-box;
+    }
   }
 `;
 export const GlobalStyles = props => <StyledGlobalStyles size={props.size} />;
