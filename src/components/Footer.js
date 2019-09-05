dc93397de57bdf3d07dca56806a6223eddc63092
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-09-05 22:52:08 +0200

    Add footer and style it with styled-components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,27 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Footer, FooterNav } from './styled-components/Footer';
+import { Link } from 'react-router-dom';
 
-function Footer() {
-  return <Fragment>Footer</Fragment>;
+function FooterComponent() {
+  return (
+    <Footer>
+      <div className="wrapper">
+        <p>
+          &copy; <Link to="/">Fridgerio</Link>, 2019
+        </p>
+        <FooterNav>
+          <ul>
+            <li>
+              <Link to="/impressum">Impressum</Link>
+            </li>
+            <li>
+              <Link to="/datenschutzerklaerung">Datenschutzerklärung</Link>
+            </li>
+          </ul>
+        </FooterNav>
+      </div>
+    </Footer>
+  );
 }
 
-export default Footer;
+export default FooterComponent;
