61c855da99fa3de3a1bd12e42a8d4fc7404b37da
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-09-18 00:05:52 +0200

    Fetch API to integrate legal notice text

--- src/components/LegalNotice.js
+++ src/components/LegalNotice.js
@@ -1,7 +1,56 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 
 function LegalNotice() {
-  return <Fragment>Impressum</Fragment>;
+  const [legal, setLegal] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchLegal();
+  }, []);
+
+  const fetchLegal = async () => {
+    setLoading(true);
+    try {
+      const url = `https://impressum-api.sklinkusch.now.sh/impressum`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setLegal(data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <Fragment>
+      {loading && <p>Inhalt wird geladen ...</p>}
+
+      {error && (
+        <p>
+          Es ist ein Fehler aufgetreten, der Inhalt kann nicht geladen werden.
+        </p>
+      )}
+
+      {legal &&
+        legal.title.map((subtitle, index) => (
+          <div key={`title-${index}`}>
+            <h2>{subtitle}</h2>
+            {typeof legal.content[index] === 'string' ? (
+              <p>{legal.content[index]}</p>
+            ) : (
+              legal.content[index].map((paragraph, subindex) => (
+                <div key={`para-${index}-${subindex}`}>
+                  <p>{paragraph}</p>
+                </div>
+              ))
+            )}
+          </div>
+        ))}
+
+      {legal && <p>{legal.source}</p>}
+    </Fragment>
+  );
 }
 
 export default LegalNotice;
