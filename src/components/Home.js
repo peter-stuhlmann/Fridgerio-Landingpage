efe35314eb58b3a62bced44d6cd95e2e97444849
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-09-17 14:05:44 +0200

    Refactor the JSX structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,36 @@
 import React, { Fragment } from 'react';
+import { StoreIcons, Smartphone } from './styled-components/Images';
+import { FlexDiv } from './styled-components/Boxes';
 
 function Home() {
   return (
     <Fragment>
-      <section>
-        <h1>Lorem isum dolor sit amet</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet.
-        </p>
+      <FlexDiv>
         <div>
-          <a href="https://play.google.com/store/apps">
-            <img src={require('../img/google-play.png')} alt="Google Play" />
-          </a>
-          <a href="https://apps.apple.com/de/app/xcode/id497799835">
-            <img src={require('../img/app-store.svg')} alt="App Store" />
-          </a>
+          <h1>Lorem isum dolor sit amet</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet.
+          </p>
+          <StoreIcons>
+            <a href="https://play.google.com/store/apps">
+              <img src={require('../img/google-play.png')} alt="Google Play" />
+            </a>
+            <a href="https://apps.apple.com/de/app/xcode/id497799835">
+              <img src={require('../img/app-store.svg')} alt="App Store" />
+            </a>
+          </StoreIcons>
         </div>
-      </section>
-      <section>
-        <img
-          src={require('../img/smartphone.svg')}
-          alt="Fridgerio Smartphone"
-        />
-      </section>
+        <Smartphone>
+          <img
+            src={require('../img/smartphone.svg')}
+            alt="Fridgerio Smartphone"
+          />
+        </Smartphone>
+      </FlexDiv>
     </Fragment>
   );
 }
