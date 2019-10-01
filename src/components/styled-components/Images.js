3753239fdd9c23e67e3d9cd2756fca95ab8d4d81
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-10-01 23:23:15 +0200

    Center the smartphone image on the mobile version

--- src/components/styled-components/Images.js
+++ src/components/styled-components/Images.js
@@ -14,11 +14,19 @@ export const StoreIcons = props => <StyledStoreIcons {...props} />;
 const StyledSmartphone = styled.section`
   flex: 0 0 40%;
 
+  @media (max-width: 768px) {
+    margin: auto;
+  }
+
   img {
     width: 300px;
     margin-right: 70px;
     display: block;
     float: right;
+
+    @media (max-width: 768px) {
+      margin: 0;
+    }
   }
 `;
 export const Smartphone = props => <StyledSmartphone {...props} />;
